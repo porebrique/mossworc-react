e9f5ab223a89345f6373dad3a513ba9b37279fa1
porebrique <porebrique@gmail.com>
2018-01-19 18:00:52 +0300

    Grid: playable config support (#8)
    Several changes in order to support playable config (one without answers)

--- src/components/grid/Cell.js
+++ src/components/grid/Cell.js
@@ -7,10 +7,12 @@ export default class extends React.Component {
 
   static propTypes = {
     id: PropTypes.string.isRequired,
+    // TODO: is never passed yet
     wordNumber: PropTypes.number,
     letter: PropTypes.string,
     isInvalid: PropTypes.bool,
     isSelected: PropTypes.bool,
+    isBlank: PropTypes.bool.isRequired,
     onSelect: PropTypes.func.isRequired
   };
 
@@ -33,11 +35,11 @@ export default class extends React.Component {
   }
 
   render() {
-    const { letter, isSelected, isInvalid, wordNumber } = this.props;
+    const { letter, isSelected, isInvalid, wordNumber, isBlank } = this.props;
     const cellClassNames = classNames('mw-grid-cell', {
       'is-invalid': isInvalid,
       'is-selected': isSelected,
-      'is-empty': !letter
+      'is-empty': isBlank
     });
 
     const cellProps = {
